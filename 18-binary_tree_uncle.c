d1e6053e10e1fe355dbe77170956b39a70d2d753
Francis Onyach <franblog7@gmail.com>
2023-03-02 11:34:06 -0800

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -23,3 +23,22 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	return (temp);
 }
+
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find uncle
+ *
+ * Return: pointer to uncle node else NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle;
+
+	if (node == NULL)
+		return (NULL);
+
+	uncle = binary_tree_sibling(node->parent);
+
+	return (uncle);
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-This repo contains the concepts and pracice 
-code/samples on binary trees in C programming.
